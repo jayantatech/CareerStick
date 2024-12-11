704d2eae6d7ef741073461e28229122b3c5003e4
Jayanta Biswas <jayantapaikpara@gmail.com>
2024-12-11 23:17:02 +0530

    fix_bakcend-middleware

--- Server/src/routes/userRoutes.ts
+++ Server/src/routes/userRoutes.ts
@@ -10,21 +10,12 @@ import {
   googleAuthCallback,
   getTokenInfo,
 } from "../controllers/user.controller";
-import verifyToken from "../middlewares/verifyToken";
-import passport from "passport";
 
 const router = Router();
 
-router.get("/", verifyToken, (req, res) => {
-  res.json({
-    success: true,
-    message: "Hello World",
-  });
-});
-
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.post("/logout", verifyToken, logoutUser);
+router.post("/logout", logoutUser);
 router.post("/verify-user/:verification_code", verifyUser);
 router.post("/forgot-password/", forgotPassword);
 router.post("/reset-password/:resetToken", resetPassword);
